48b7669a28a5ebbc5dbb39850bad5743ff69d9f0
Quyana Barrow <quyanabarrow@Quyanas-MacBook-Pro.local>
2023-04-18 18:30:04 -0400

    retrieve multiple past data and returning in bootstrap cards

--- src/components/sub-components/GetKino.jsx
+++ src/components/sub-components/GetKino.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Card } from "react-bootstrap";
 
-export default function Getkino() {
-  const url = "https://puertorico.secondchancebonuszone.com/kino/past_drawings.php";
+export default function GetKino() {
+//   const url = "https://puertorico.secondchancebonuszone.com/kino/past_drawings.php";
+  const url = "https://puertorico.secondchancebonuszone.com/kino/past_drawings.php?drawid=601081&number=20&sort=asc"
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -11,43 +13,35 @@ export default function Getkino() {
     loadDraw();
   }, []);
 
-  const loadDraw = async (itemGameNumber, itemBonus) => {
+  const loadDraw = async () => {
     try {
-      const response = await axios.get(url, {
-        params: {
-          gameNumber: itemGameNumber,
-          bonus: itemBonus,
-        },
-      });
+      const response = await axios.get(url);
       setData(response.data);
       setLoading(false);
-      console.log(response.data);
+      console.log(response.data.json);
     } catch (error) {
       console.log(error);
       setError(error);
     }
   };
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <>
-      <div>
-        <h1>kino get data</h1>
-      </div>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {!loading && !error && (
-        <div>
-          {data.map((item, index) => {
-            return (
-              <div key={index}>
-                <h2>gameNumber: {item.gameNumber}</h2>
-                <h3>bonus: {item.bonus}</h3>
-                <h3>drawNumbers: {item.drawNumbers}</h3>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </>
+    <div>
+      <h1>Kino past drawings</h1>
+      {data.map((item, index) => (
+        <Card key={index} className="my-4">
+          <Card.Header>
+            <h4 className="mb-0">Game Number: {item.gameNumber}</h4>
+          </Card.Header>
+          <Card.Body>
+            <p className="mb-0">Bonus: {item.bonus}</p>
+            <p className="mb-0">Draw Numbers: {item.drawNumbers.join(", ")}</p>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   );
 }
